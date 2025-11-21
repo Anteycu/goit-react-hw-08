255a71929a1e8338cc13a0a58e6ff279e1f17874
Mykola T <anteycu@gmail.com>
2025-11-21 02:55:38 +0200

    add env variable for token, fix api for alive backend

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,8 +1,16 @@
+// import { useDispatch } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactsList from "../../components/ContactsList/ContactsList";
 import SearchBox from "../../components/SearchBox/SearchBox";
+// import { useEffect } from "react";
 
 const Contacts = () => {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch();
+  // }, [dispatch]);
+
   return (
     <div>
       <ContactForm />
--- src/services/contacts-api.js
+++ src/services/contacts-api.js
@@ -1,58 +1,54 @@
 import axios from "axios";
+const apiToken = import.meta.env.VITE_API_TOKEN;
+const apiUrl = import.meta.env.VITE_API_URL;
 
-axios.defaults.baseURL("https://connections-api.goit.global");
+axios.defaults.baseURL = apiUrl;
 
 const contacts = axios.create({
-  baseURL: "https://connections-api.goit.global",
-  headers: { Authorization: `Bearer ${"AUTH_TOKEN"}` },
+  baseURL: apiUrl,
+  headers: { Authorization: `Bearer ${apiToken}` },
 });
 
 // not for all post and need for 1 patch
 // axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export const register = (user) => {
-  axios.post("/users/signup", user);
+  axios.post("/auth", user);
 };
 
-// {
-//     "name": "Adrian Cross",
-//     "email": "across@mail.com",
-//     "password": "examplepwd12345"
-//   }
+// for my own backend in future
+// export const login = (user) => {
+//   axios.post("/users/login", user);
+// };
 
-export const login = (user) => {
-  axios.post("/users/login", user);
-};
+// export const logout = () => {
+//   axios.post("/users/logout", {
+//     header: { Authorization: `Bearer ${apiToken}` },
+//   });
+// };
 
-// {
-//     "email": "string",
-//     "password": "string"
-//   }
-
-export const logout = () => {
-  axios.post("/users/logout", {
-    header: { Authorization: `Bearer ${"AUTH_TOKEN"}` },
-  });
-};
-
-export const refresh = () => {
-  axios.get("/users/current", {
-    header: { Authorization: `Bearer ${"AUTH_TOKEN"}` },
-  });
-};
+// export const refresh = () => {
+//   axios.get("/users/current", {
+//     header: { Authorization: `Bearer ${apiToken}` },
+//   });
+// };
 
 export const getContacts = () => {
-  contacts.get("/contacts");
+  contacts.get("/notes");
+};
+
+export const getContact = (id) => {
+  contacts.get(`/notes/${id}`);
 };
 
 export const addContact = (contact) => {
-  contacts.get("contact", contact);
+  contacts.post("/notes", contact);
 };
 
 export const updateContact = (contact, id) => {
-  contacts.patch(`/contacts/${id}`, contact);
+  contacts.patch(`/notes/${id}`, contact);
 };
 
 export const deleteContact = (id) => {
-  contacts.delete(`/contacts/${id}`);
+  contacts.delete(`/notes/${id}`);
 };
