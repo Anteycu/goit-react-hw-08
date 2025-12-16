09ee993068c92b024f8e06526cdd6a5016d63d50
Mykola T. <anteycu@gmail.com>
2025-12-16 18:12:51 +0200

    add persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,17 +11,9 @@ import Modal from "./Modal/Modal";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  const [clicks, setClicks] = useState(() => {
-    const savedClicks = window.localStorage.getItem("saved-clicks");
-    if (savedClicks !== null) {
-      return Number(savedClicks);
-    }
-    return 0;
-  });
 
   useEffect(() => {
-    window.localStorage.setItem("saved-clicks", clicks);
-  }, [clicks]);
+  }, []);
 
   return (
     <Layout>
@@ -38,10 +30,6 @@ function App() {
           {isOpen ? "Close" : "Open"}
         </button>
         {isOpen && <Modal />}
-        <button onClick={() => setClicks(clicks + 1)}>
-          You clicked {clicks} times
-        </button>
-        <button onClick={() => setClicks(0)}>Reset</button>
       </div>
     </Layout>
   );
